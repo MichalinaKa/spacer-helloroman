e8d4483ae4e1d4caef1a04d1343d589cee8a421f
MichalinaKa <kaminska.michalinaa@gmail.com>
2020-02-21 11:37:48 +0100

    media queries for views

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,22 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Search from "../views/Search.vue";
+// import Vue from "vue";
+// import VueRouter from "vue-router";
 
-Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Search",
-    component: Search
-  }
+// Vue.use(VueRouter);
 
-];
+// const routes = [
+//   {
+//     path: "/",
+//     name: "Search",
+//     component: Search
+//   }
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
-});
+// ];
 
-export default router;
+// const router = new VueRouter({
+//   mode: "history",
+//   base: process.env.BASE_URL,
+//   routes
+// });
+
+// export default router;
